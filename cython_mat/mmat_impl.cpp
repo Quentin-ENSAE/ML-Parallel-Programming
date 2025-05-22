4526786f45f7ea51886527e87a50b0af1607ad18
Joris <joris.chenevas@free.fr>
2025-05-22 20:51:34 +0200

    code cleaning + commenting

--- cython_mat/mmat_impl.cpp
+++ cython_mat/mmat_impl.cpp
@@ -18,7 +18,7 @@ template <typename DTYPE> inline DTYPE &at(DTYPE *p, int cols, int i, int j) {
   return p[i * cols + j];
 }
 
-// Multiplication de matrices plates par blocs
+// Calcul de l'attention par blocs
 template <typename DTYPE>
 void BlockMatrixMultiply(const DTYPE *A, const DTYPE *B, DTYPE *C, const DTYPE *D, DTYPE *E, int n, int m,
                          int p, int q, int r, int block_size) {
@@ -26,11 +26,8 @@ void BlockMatrixMultiply(const DTYPE *A, const DTYPE *B, DTYPE *C, const DTYPE *
   double max[n] = {};
   std::fill_n(max, block_size, -DBL_MAX);
   #pragma omp parallel for collapse(2) schedule(dynamic)
+  // Tout d'abord on multiplie les deux première matrices
   for (int i = 0; i < n; i += block_size) {
-    //double exp_sum[block_size] = {};
-    //double max_sum[block_size] = {};
-    //std::fill_n(max_sum, block_size, -1);
-
     for (int j = 0; j < p; j += block_size) {
       for (int k = 0; k < m; k += block_size) {
         // Sous-bloc
@@ -38,37 +35,36 @@ void BlockMatrixMultiply(const DTYPE *A, const DTYPE *B, DTYPE *C, const DTYPE *
           for (int jj = j; jj < std::min(j + block_size, p); ++jj) {
             double sum = 0.0;
             for (int kk = k; kk < std::min(k + block_size, m); ++kk) {
+            // On inverse jj et kk pour B, pour transposer la matrice
+            // On applique également directement la division par la racine de la dimension
               sum += at(A, m, ii, kk) * at(B, p, jj, kk);
             }
             at(C, p, ii, jj) += sum/dk;
             if(max[ii] < at(C, p, ii, jj)){
                 max[ii] = at(C, p, ii, jj);
             }
-            //exp_sum[ii] += exp(at(C, p, ii, jj));
-            //if (val > max_val[ii - i]) {
-            //   max_val[ii - i] = val;
-            //}
           }
-
-
         }
       }
     }
   }
+  //On calcul le soft max
   #pragma omp parallel for schedule(dynamic)
   for(int i = 0; i < n; ++i){
+    // On récupère le maximum de chaque ligne
     double exp_sum = 0;
     for(int j = 0; j< p; ++j){
         double exp_val = exp(at(C, p, i, j) - max[i]);
         at(C, p, i, j) = exp_val;
         exp_sum += exp_val;
     }
+    // Et on applique le soft max à toutes les valeurs
     for(int j = 0; j< p; ++j){
         at(C, p, i, j) = at(C, p, i, j) / exp_sum;
     }
-    }
+  }
 
-
+  //On calcul maintenant la multiplication final de matrice
   #pragma omp parallel for collapse(2) schedule(dynamic)
   for (int i = 0; i < n; i += block_size) {
     for (int j = 0; j < q; j += block_size) {
@@ -88,112 +84,12 @@ void BlockMatrixMultiply(const DTYPE *A, const DTYPE *B, DTYPE *C, const DTYPE *
   }
 }
 
-void BlockMatrixMultiplyAVX(const double *A, const double *B, double *C, int n,
-                            int m, int p, int block_size) {
-#pragma omp parallel for collapse(2) schedule(dynamic)
-  for (int i = 0; i < n; i += block_size) {
-    for (int j = 0; j < p; j += block_size) {
-      for (int k = 0; k < m; k += block_size) {
-        int i_max = std::min(i + block_size, n);
-        int j_max = std::min(j + block_size, p);
-        int k_max = std::min(k + block_size, m);
-
-        for (int ii = i; ii < i_max; ++ii) {
-          int jj = j;
-          // Vectorisé par paquets de 4 colonnes
-          for (; jj + 3 < j_max; jj += 4) {
-            // Charge les 4 valeurs C[ii][jj..jj+3]
-            __m256d c_vec = _mm256_loadu_pd(&C[ii * p + jj]);
-
-            // Accumulation sur k
-            for (int kk = k; kk < k_max; ++kk) {
-              // Broadcast A[ii][kk] dans un vecteur
-              __m256d a_vec = _mm256_set1_pd(A[ii * m + kk]);
-              // Charge B[kk][jj..jj+3]
-              __m256d b_vec = _mm256_loadu_pd(&B[kk * p + jj]);
-              // FMA : c_vec += a_vec * b_vec
-#ifdef __FMA__
-              c_vec = _mm256_fmadd_pd(a_vec, b_vec, c_vec);
-#else
-              c_vec = _mm256_add_pd(_mm256_mul_pd(a_vec, b_vec), c_vec);
-#endif              
-            }
-            // Stocke le résultat
-            _mm256_storeu_pd(&C[ii * p + jj], c_vec);
-          }
-          // Reste scalaire pour les colonnes non alignées
-          for (; jj < j_max; ++jj) {
-            double sum = 0.0;
-            for (int kk = k; kk < k_max; ++kk) {
-              sum += A[ii * m + kk] * B[kk * p + jj];
-            }
-            C[ii * p + jj] += sum;
-          }
-        }
-      }
-    }
-  }
-}
-
-void BlockMatrixMultiplyAVX(const float *A, const float *B, float *C, int n,
-                            int m, int p, int block_size) {
-#pragma omp parallel for collapse(2) schedule(dynamic)
-  for (int i = 0; i < n; i += block_size) {
-    for (int j = 0; j < p; j += block_size) {
-      for (int k = 0; k < m; k += block_size) {
-        int i_max = std::min(i + block_size, n);
-        int j_max = std::min(j + block_size, p);
-        int k_max = std::min(k + block_size, m);
-
-        for (int ii = i; ii < i_max; ++ii) {
-          int jj = j;
-          // Vectorisé par paquets de 8 colonnes (float)
-          for (; jj + 7 < j_max; jj += 8) {
-            // Charge C[ii][jj..jj+7]
-            __m256 c_vec = _mm256_loadu_ps(&C[ii * p + jj]);
-
-            // Accumulation sur k
-            for (int kk = k; kk < k_max; ++kk) {
-              // Broadcast A[ii][kk] dans un vecteur
-              __m256 a_vec = _mm256_set1_ps(A[ii * m + kk]);
-              // Charge B[kk][jj..jj+7]
-              __m256 b_vec = _mm256_loadu_ps(&B[kk * p + jj]);
-              // FMA : c_vec += a_vec * b_vec
-#ifdef __FMA__
-              c_vec = _mm256_fmadd_ps(a_vec, b_vec, c_vec);
-#else
-              c_vec = _mm256_add_ps(_mm256_mul_ps(a_vec, b_vec), c_vec);
-#endif              
-            }
-            // Stocke le résultat
-            _mm256_storeu_ps(&C[ii * p + jj], c_vec);
-          }
-          // Reste scalaire pour les colonnes non multiples de 8
-          for (; jj < j_max; ++jj) {
-            float sum = 0.0f;
-            for (int kk = k; kk < k_max; ++kk) {
-              sum += A[ii * m + kk] * B[kk * p + jj];
-            }
-            C[ii * p + jj] += sum;
-          }
-        }
-      }
-    }
-  }
-}
-
 void mmat_impl_cpp(int n_row, int n_col, int k, int v, int l, const float *p1,
                    const float *p2, float *res, const float *p3, float *res2, int block_size, int version) {
-  if (version == 0)
-    BlockMatrixMultiply(p1, p2, res, p3, res2, n_row, k, n_col, v, l, block_size);
-  else
-    BlockMatrixMultiplyAVX(p1, p2, res, n_row, k, n_col, block_size);
+   BlockMatrixMultiply(p1, p2, res, p3, res2, n_row, k, n_col, v, l, block_size);
 }
 
 void mmat_impl_cpp(int n_row, int n_col, int k, int v, int l, const double *p1,
                    const double *p2, double *res, const double *p3, double *res2, int block_size, int version) {
-  if (version == 0)
-    BlockMatrixMultiply(p1, p2, res, p3, res2, n_row, k, n_col, v, l, block_size);
-  else
-    BlockMatrixMultiplyAVX(p1, p2, res, n_row, k, n_col, block_size);
+   BlockMatrixMultiply(p1, p2, res, p3, res2, n_row, k, n_col, v, l, block_size);
 }
